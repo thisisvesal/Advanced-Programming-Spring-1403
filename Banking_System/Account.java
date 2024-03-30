0f83b04060f5db49526517448cc5dc4ab12b62b0
thisisvesal <vesal.ea84@gmail.com>
2024-03-30 18:15:17 +0430

    Automized account number setting

--- Banking_System/Account.java
+++ Banking_System/Account.java
@@ -1,20 +1,30 @@
+import java.util.Random;
+
 public class Account {
-    private String accountNumber;
+    public final String accountNumber;
     private int balance;
     public final Customer accountOwner;
     private Branch branch;
 
     public Account(Customer accountOwner, Branch branch) {
+        Random random = new Random();
         this.accountOwner = accountOwner;
         this.branch = branch;
         accountOwner.setAccount(this);
-    }
 
-    public void setAccountNumber(String accountNumber) {
-        this.accountNumber = accountNumber;
-    }
-    public String getAccountNumber() {
-        return accountNumber;
+        String personsCode;
+        if (accountOwner.IDInCentralBanking < 10) {
+            personsCode = "000" + accountOwner.IDInCentralBanking;
+        } else if (accountOwner.IDInCentralBanking < 100) {
+            personsCode = "00" + accountOwner.IDInCentralBanking;
+        } else if (accountOwner.IDInCentralBanking < 1000) {
+            personsCode = "0" + accountOwner.IDInCentralBanking;
+        } else {
+            personsCode = "" + accountOwner.IDInCentralBanking;
+        }
+
+        accountNumber = branch.bank.getCodeOfBank() + branch.getCode() + personsCode
+                + (random.nextInt(9999 - 1000 + 1) + 1000);
     }
 
     public void deposit(int amount) {
@@ -24,6 +34,7 @@ public class Account {
             System.out.println("Deposit amount cannot be negative");
         }
     }
+
     public void withdraw(int amount) {
         if (amount < 0) {
             System.out.println("Withdrawal amount cannot be negative");
@@ -33,6 +44,9 @@ public class Account {
             System.out.println("The customer does not have enough balance for this withdrawal");
         }
     }
+
+    // There is no setBalance method. Use deposit and withdraw instead.
+    
     public int getBalance() {
         return balance;
     }
@@ -41,6 +55,7 @@ public class Account {
         this.branch = branch;
         branch.accountGenerator(accountOwner);
     }
+
     public Branch getBranch() {
         return branch;
     }
