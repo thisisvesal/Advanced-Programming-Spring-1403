23626461b8f2a31808d938871df1528a9c7d2859
thisisvesal <vesal.ea84@gmail.com>
2024-05-19 01:00:17 +0430

    Removed redundant code

--- VU/Questions/Course.java
+++ VU/Questions/Course.java
@@ -4,22 +4,18 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
-import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseListener;
 import java.util.ArrayList;
 
-import javax.swing.ButtonGroup;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JMenuItem;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JPopupMenu;
-import javax.swing.JRadioButton;
 
 import Frames.CoursePage;
 import Frames.ExamPage;
